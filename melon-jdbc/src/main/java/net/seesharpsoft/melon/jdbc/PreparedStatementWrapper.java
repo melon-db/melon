10d33100d759c5718e979d8120c687ba0907c019
GeeK <mail@seesharpsoft.net>
2018-09-16 22:17:49 +0200

    [FEATURE] auto-commit support

---
+++ melon-jdbc/src/main/java/net/seesharpsoft/melon/jdbc/PreparedStatementWrapper.java
@@ -0,0 +1,582 @@
+package net.seesharpsoft.melon.jdbc;
+
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.util.Calendar;
+
+public class PreparedStatementWrapper implements PreparedStatement{
+    private Statement delegate;
+
+    protected PreparedStatement getPSDelegate() {
+        return (PreparedStatement)delegate;
+    }
+
+    protected Statement getDelegate() {
+        return delegate;
+    }
+    
+    public PreparedStatementWrapper(Statement delegate) {
+        this.delegate = delegate;
+    }
+    
+    @Override
+    public<T> T unwrap(Class<T> iface) throws SQLException {
+        return getDelegate().unwrap(iface);
+    }
+
+    @Override
+    public ResultSet executeQuery(String sql) throws SQLException {
+        return getDelegate().executeQuery(sql);
+    }
+
+    @Override
+    public ResultSet executeQuery() throws SQLException {
+        return getPSDelegate().executeQuery();
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return getDelegate().isWrapperFor(iface);
+    }
+
+    @Override
+    public int executeUpdate(String sql) throws SQLException {
+        return getDelegate().executeUpdate(sql);
+    }
+
+    @Override
+    public int executeUpdate() throws SQLException {
+        return getPSDelegate().executeUpdate();
+    }
+
+    @Override
+    public void setNull(int parameterIndex, int sqlType) throws SQLException {
+        getPSDelegate().setNull(parameterIndex, sqlType);
+    }
+
+    @Override
+    public void close() throws SQLException {
+        getDelegate().close();
+    }
+
+    @Override
+    public int getMaxFieldSize() throws SQLException {
+        return getDelegate().getMaxFieldSize();
+    }
+
+    @Override
+    public void setBoolean(int parameterIndex, boolean x) throws SQLException {
+        getPSDelegate().setBoolean(parameterIndex, x);
+    }
+
+    @Override
+    public void setByte(int parameterIndex, byte x) throws SQLException {
+        getPSDelegate().setByte(parameterIndex, x);
+    }
+
+    @Override
+    public void setMaxFieldSize(int max) throws SQLException {
+        getDelegate().setMaxFieldSize(max);
+    }
+
+    @Override
+    public void setShort(int parameterIndex, short x) throws SQLException {
+        getPSDelegate().setShort(parameterIndex, x);
+    }
+
+    @Override
+    public int getMaxRows() throws SQLException {
+        return getDelegate().getMaxRows();
+    }
+
+    @Override
+    public void setInt(int parameterIndex, int x) throws SQLException {
+        getPSDelegate().setInt(parameterIndex, x);
+    }
+
+    @Override
+    public void setMaxRows(int max) throws SQLException {
+        getDelegate().setMaxRows(max);
+    }
+
+    @Override
+    public void setLong(int parameterIndex, long x) throws SQLException {
+        getPSDelegate().setLong(parameterIndex, x);
+    }
+
+    @Override
+    public void setEscapeProcessing(boolean enable) throws SQLException {
+        getDelegate().setEscapeProcessing(enable);
+    }
+
+    @Override
+    public void setFloat(int parameterIndex, float x) throws SQLException {
+        getPSDelegate().setFloat(parameterIndex, x);
+    }
+
+    @Override
+    public void setDouble(int parameterIndex, double x) throws SQLException {
+        getPSDelegate().setDouble(parameterIndex, x);
+    }
+
+    @Override
+    public int getQueryTimeout() throws SQLException {
+        return getDelegate().getQueryTimeout();
+    }
+
+    @Override
+    public void setQueryTimeout(int seconds) throws SQLException {
+        getDelegate().setQueryTimeout(seconds);
+    }
+
+    @Override
+    public void setBigDecimal(int parameterIndex, BigDecimal x)
+            throws SQLException {
+        getPSDelegate().setBigDecimal(parameterIndex, x);
+    }
+
+    @Override
+    public void setString(int parameterIndex, String x) throws SQLException {
+        getPSDelegate().setString(parameterIndex, x);
+    }
+
+    @Override
+    public void setBytes(int parameterIndex, byte[] x) throws SQLException {
+        getPSDelegate().setBytes(parameterIndex, x);
+    }
+
+    @Override
+    public void cancel() throws SQLException {
+        getDelegate().cancel();
+    }
+
+    @Override
+    public SQLWarning getWarnings() throws SQLException {
+        return getDelegate().getWarnings();
+    }
+
+    @Override
+    public void setDate(int parameterIndex, Date x) throws SQLException {
+        getPSDelegate().setDate(parameterIndex, x);
+    }
+
+    @Override
+    public void setTime(int parameterIndex, Time x) throws SQLException {
+        getPSDelegate().setTime(parameterIndex, x);
+    }
+
+    @Override
+    public void clearWarnings() throws SQLException {
+        getDelegate().clearWarnings();
+    }
+
+    @Override
+    public void setCursorName(String name) throws SQLException {
+        getDelegate().setCursorName(name);
+    }
+
+    @Override
+    public void setTimestamp(int parameterIndex, Timestamp x)
+            throws SQLException {
+        getPSDelegate().setTimestamp(parameterIndex, x);
+    }
+
+    @Override
+    public void setAsciiStream(int parameterIndex, InputStream x, int length)
+            throws SQLException {
+        getPSDelegate().setAsciiStream(parameterIndex, x, length);
+    }
+
+    @Override
+    public boolean execute(String sql) throws SQLException {
+        return getDelegate().execute(sql);
+    }
+
+    @Deprecated
+    public void setUnicodeStream(int parameterIndex, InputStream x, int length)
+            throws SQLException {
+        getPSDelegate().setUnicodeStream(parameterIndex, x, length);
+    }
+
+    @Override
+    public ResultSet getResultSet() throws SQLException {
+        return getDelegate().getResultSet();
+    }
+
+    @Override
+    public void setBinaryStream(int parameterIndex, InputStream x, int length)
+            throws SQLException {
+        getPSDelegate().setBinaryStream(parameterIndex, x, length);
+    }
+
+    @Override
+    public int getUpdateCount() throws SQLException {
+        return getDelegate().getUpdateCount();
+    }
+
+    @Override
+    public boolean getMoreResults() throws SQLException {
+        return getDelegate().getMoreResults();
+    }
+
+    @Override
+    public void clearParameters() throws SQLException {
+        getPSDelegate().clearParameters();
+    }
+
+    @Override
+    public void setObject(int parameterIndex, Object x, int targetSqlType)
+            throws SQLException {
+        getPSDelegate().setObject(parameterIndex, x, targetSqlType);
+    }
+
+    @Override
+    public void setFetchDirection(int direction) throws SQLException {
+        getDelegate().setFetchDirection(direction);
+    }
+
+    @Override
+    public int getFetchDirection() throws SQLException {
+        return getDelegate().getFetchDirection();
+    }
+
+    @Override
+    public void setObject(int parameterIndex, Object x) throws SQLException {
+        getPSDelegate().setObject(parameterIndex, x);
+    }
+
+    @Override
+    public void setFetchSize(int rows) throws SQLException {
+        getDelegate().setFetchSize(rows);
+    }
+
+    @Override
+    public int getFetchSize() throws SQLException {
+        return getDelegate().getFetchSize();
+    }
+
+    @Override
+    public int getResultSetConcurrency() throws SQLException {
+        return getDelegate().getResultSetConcurrency();
+    }
+
+    @Override
+    public boolean execute() throws SQLException {
+        return getPSDelegate().execute();
+    }
+
+    @Override
+    public int getResultSetType() throws SQLException {
+        return getDelegate().getResultSetType();
+    }
+
+    @Override
+    public void addBatch(String sql) throws SQLException {
+        getDelegate().addBatch(sql);
+    }
+
+    @Override
+    public void clearBatch() throws SQLException {
+        getDelegate().clearBatch();
+    }
+
+    @Override
+    public void addBatch() throws SQLException {
+        getPSDelegate().addBatch();
+    }
+
+    @Override
+    public int[] executeBatch() throws SQLException {
+        return getDelegate().executeBatch();
+    }
+
+    @Override
+    public void setCharacterStream(int parameterIndex, Reader reader, int length)
+            throws SQLException {
+        getPSDelegate().setCharacterStream(parameterIndex, reader, length);
+    }
+
+    @Override
+    public void setRef(int parameterIndex, Ref x) throws SQLException {
+        getPSDelegate().setRef(parameterIndex, x);
+    }
+
+    @Override
+    public void setBlob(int parameterIndex, Blob x) throws SQLException {
+        getPSDelegate().setBlob(parameterIndex, x);
+    }
+
+    @Override
+    public void setClob(int parameterIndex, Clob x) throws SQLException {
+        getPSDelegate().setClob(parameterIndex, x);
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return getDelegate().getConnection();
+    }
+
+    @Override
+    public void setArray(int parameterIndex, Array x) throws SQLException {
+        getPSDelegate().setArray(parameterIndex, x);
+    }
+
+    @Override
+    public ResultSetMetaData getMetaData() throws SQLException {
+        return getPSDelegate().getMetaData();
+    }
+
+    @Override
+    public boolean getMoreResults(int current) throws SQLException {
+        return getDelegate().getMoreResults(current);
+    }
+
+    @Override
+    public void setDate(int parameterIndex, Date x, Calendar cal)
+            throws SQLException {
+        getPSDelegate().setDate(parameterIndex, x, cal);
+    }
+
+    @Override
+    public ResultSet getGeneratedKeys() throws SQLException {
+        return getDelegate().getGeneratedKeys();
+    }
+
+    @Override
+    public void setTime(int parameterIndex, Time x, Calendar cal)
+            throws SQLException {
+        getPSDelegate().setTime(parameterIndex, x, cal);
+    }
+
+    @Override
+    public int executeUpdate(String sql, int autoGeneratedKeys)
+            throws SQLException {
+        return getDelegate().executeUpdate(sql, autoGeneratedKeys);
+    }
+
+    @Override
+    public void setTimestamp(int parameterIndex, Timestamp x, Calendar cal)
+            throws SQLException {
+        getPSDelegate().setTimestamp(parameterIndex, x, cal);
+    }
+
+    @Override
+    public void setNull(int parameterIndex, int sqlType, String typeName)
+            throws SQLException {
+        getPSDelegate().setNull(parameterIndex, sqlType, typeName);
+    }
+
+    @Override
+    public int executeUpdate(String sql, int[] columnIndexes)
+            throws SQLException {
+        return getDelegate().executeUpdate(sql, columnIndexes);
+    }
+
+    @Override
+    public void setURL(int parameterIndex, URL x) throws SQLException {
+        getPSDelegate().setURL(parameterIndex, x);
+    }
+
+    @Override
+    public int executeUpdate(String sql, String[] columnNames)
+            throws SQLException {
+        return getDelegate().executeUpdate(sql, columnNames);
+    }
+
+    @Override
+    public ParameterMetaData getParameterMetaData() throws SQLException {
+        return getPSDelegate().getParameterMetaData();
+    }
+
+    @Override
+    public void setRowId(int parameterIndex, RowId x) throws SQLException {
+        getPSDelegate().setRowId(parameterIndex, x);
+    }
+
+    @Override
+    public void setNString(int parameterIndex, String value)
+            throws SQLException {
+        getPSDelegate().setNString(parameterIndex, value);
+    }
+
+    @Override
+    public boolean execute(String sql, int autoGeneratedKeys)
+            throws SQLException {
+        return getDelegate().execute(sql, autoGeneratedKeys);
+    }
+
+    @Override
+    public void setNCharacterStream(int parameterIndex, Reader value,
+                                    long length) throws SQLException {
+        getPSDelegate().setNCharacterStream(parameterIndex, value, length);
+    }
+
+    @Override
+    public void setNClob(int parameterIndex, NClob value) throws SQLException {
+        getPSDelegate().setNClob(parameterIndex, value);
+    }
+
+    @Override
+    public void setClob(int parameterIndex, Reader reader, long length)
+            throws SQLException {
+        getPSDelegate().setClob(parameterIndex, reader, length);
+    }
+
+    @Override
+    public boolean execute(String sql, int[] columnIndexes) throws SQLException {
+        return getDelegate().execute(sql, columnIndexes);
+    }
+
+    @Override
+    public void setBlob(int parameterIndex, InputStream inputStream, long length)
+            throws SQLException {
+        getPSDelegate().setBlob(parameterIndex, inputStream, length);
+    }
+
+    @Override
+    public void setNClob(int parameterIndex, Reader reader, long length)
+            throws SQLException {
+        getPSDelegate().setNClob(parameterIndex, reader, length);
+    }
+
+    @Override
+    public boolean execute(String sql, String[] columnNames)
+            throws SQLException {
+        return getDelegate().execute(sql, columnNames);
+    }
+
+    @Override
+    public void setSQLXML(int parameterIndex, SQLXML xmlObject)
+            throws SQLException {
+        getPSDelegate().setSQLXML(parameterIndex, xmlObject);
+    }
+
+    @Override
+    public void setObject(int parameterIndex, Object x, int targetSqlType,
+                          int scaleOrLength) throws SQLException {
+        getPSDelegate().setObject(parameterIndex, x, targetSqlType, scaleOrLength);
+    }
+
+    @Override
+    public int getResultSetHoldability() throws SQLException {
+        return getDelegate().getResultSetHoldability();
+    }
+
+    @Override
+    public boolean isClosed() throws SQLException {
+        return getDelegate().isClosed();
+    }
+
+    @Override
+    public void setPoolable(boolean poolable) throws SQLException {
+        getDelegate().setPoolable(poolable);
+    }
+
+    @Override
+    public boolean isPoolable() throws SQLException {
+        return getDelegate().isPoolable();
+    }
+
+    @Override
+    public void closeOnCompletion() throws SQLException {
+        getDelegate().closeOnCompletion();
+    }
+
+    @Override
+    public void setAsciiStream(int parameterIndex, InputStream x, long length)
+            throws SQLException {
+        getPSDelegate().setAsciiStream(parameterIndex, x, length);
+    }
+
+    @Override
+    public boolean isCloseOnCompletion() throws SQLException {
+        return getDelegate().isCloseOnCompletion();
+    }
+
+    @Override
+    public void setBinaryStream(int parameterIndex, InputStream x, long length)
+            throws SQLException {
+        getPSDelegate().setBinaryStream(parameterIndex, x, length);
+    }
+
+    @Override
+    public void setCharacterStream(int parameterIndex, Reader reader,
+                                   long length) throws SQLException {
+        getPSDelegate().setCharacterStream(parameterIndex, reader, length);
+    }
+
+    @Override
+    public void setAsciiStream(int parameterIndex, InputStream x)
+            throws SQLException {
+        getPSDelegate().setAsciiStream(parameterIndex, x);
+    }
+
+    @Override
+    public void setBinaryStream(int parameterIndex, InputStream x)
+            throws SQLException {
+        getPSDelegate().setBinaryStream(parameterIndex, x);
+    }
+
+    @Override
+    public void setCharacterStream(int parameterIndex, Reader reader)
+            throws SQLException {
+        getPSDelegate().setCharacterStream(parameterIndex, reader);
+    }
+
+    @Override
+    public void setNCharacterStream(int parameterIndex, Reader value)
+            throws SQLException {
+        getPSDelegate().setNCharacterStream(parameterIndex, value);
+    }
+
+    @Override
+    public void setClob(int parameterIndex, Reader reader) throws SQLException {
+        getPSDelegate().setClob(parameterIndex, reader);
+    }
+
+    @Override
+    public void setBlob(int parameterIndex, InputStream inputStream)
+            throws SQLException {
+        getPSDelegate().setBlob(parameterIndex, inputStream);
+    }
+
+    @Override
+    public void setNClob(int parameterIndex, Reader reader) throws SQLException {
+        getPSDelegate().setNClob(parameterIndex, reader);
+    }
+
+    @Override
+    public long executeLargeUpdate() throws SQLException {
+        return getPSDelegate().executeLargeUpdate();
+    }
+
+    @Override
+    public long[] executeLargeBatch() throws SQLException {
+        return getDelegate().executeLargeBatch();
+    }
+
+    @Override
+    public long executeLargeUpdate(String sql) throws SQLException {
+        return getDelegate().executeLargeUpdate(sql);
+    }
+
+    @Override
+    public long executeLargeUpdate(String sql, int autoGeneratedKeys)
+            throws SQLException {
+        return getDelegate().executeLargeUpdate(sql, autoGeneratedKeys);
+    }
+
+    @Override
+    public long executeLargeUpdate(String sql, int columnIndexes[]) throws SQLException {
+        return getDelegate().executeLargeUpdate(sql, columnIndexes);
+    }
+
+    @Override
+    public long executeLargeUpdate(String sql, String columnNames[])
+            throws SQLException {
+        return getDelegate().executeLargeUpdate(sql, columnNames);
+    }
+}
